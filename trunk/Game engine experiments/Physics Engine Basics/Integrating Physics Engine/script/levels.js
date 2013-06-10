ec563149ca568573a20617ff5735d87b888aaf03
nikita.moshensky@gmail.com <nikita.moshensky@gmail.com@bab1d769-ab47-f41f-4f00-7e570b04f98a>
2013-06-10 12:13:06 +0000

    basic experiments

---
+++ trunk/Game engine experiments/Physics Engine Basics/Integrating Physics Engine/script/levels.js
@@ -0,0 +1,73 @@
+﻿var levels = {
+    data: [
+    { // First level
+        foreground: 'desert-foreground',
+        background: 'clouds-background',
+        entities: [
+            { type: "ground", name: "dirt", x: 500, y: 440, width: 1000, height: 20, isStatic: true },
+            { type: "ground", name: "wood", x: 180, y: 390, width: 40, height: 80, isStatic: true },
+            { type: "block", name: "wood", x: 520, y: 375, angle: 90, width: 100, height: 25 },
+            { type: "block", name: "glass", x: 520, y: 275, angle: 90, width: 100, height: 25 },
+            { type: "villain", name: "burger", x: 520, y: 200, calories: 590 },
+            { type: "block", name: "wood", x: 620, y: 375, angle: 90, width: 100, height: 25 },
+            { type: "block", name: "glass", x: 620, y: 275, angle: 90, width: 100, height: 25 },
+            { type: "villain", name: "fries", x: 620, y: 200, calories: 420 },
+            { type: "hero", name: "orange", x: 90, y: 410 },
+            { type: "hero", name: "apple", x: 150, y: 410 },
+        ]
+    },
+    { // Second level
+        foreground: 'desert-foreground',
+        background: 'clouds-background',
+        entities: [
+            { type: "ground", name: "dirt", x: 500, y: 440, width: 1000, height: 20, isStatic: true },
+            { type: "ground", name: "wood", x: 180, y: 390, width: 40, height: 80, isStatic: true },
+            { type: "block", name: "wood", x: 820, y: 375, angle: 90, width: 100, height: 25 },
+            { type: "block", name: "wood", x: 720, y: 375, angle: 90, width: 100, height: 25 },
+            { type: "block", name: "wood", x: 620, y: 375, angle: 90, width: 100, height: 25 },
+            { type: "block", name: "glass", x: 670, y: 310, width: 100, height: 25 },
+            { type: "block", name: "glass", x: 770, y: 310, width: 100, height: 25 },
+            { type: "block", name: "glass", x: 670, y: 248, angle: 90, width: 100, height: 25 },
+            { type: "block", name: "glass", x: 770, y: 248, angle: 90, width: 100, height: 25 },
+            { type: "block", name: "wood", x: 720, y: 180, width: 100, height: 25 },
+            { type: "villain", name: "burger", x: 715, y: 160, calories: 590 },
+            { type: "villain", name: "fries", x: 670, y: 400, calories: 420 },
+            { type: "villain", name: "sodacan", x: 765, y: 395, calories: 150 },
+            { type: "hero", name: "strawberry", x: 40, y: 420 },
+            { type: "hero", name: "orange", x: 90, y: 410 },
+            { type: "hero", name: "apple", x: 150, y: 410 },
+        ]
+    }
+    ],
+
+    // Load all data and images for a specific level
+    load:function(number){
+        //Initialize Box2D world whenever a level is loaded
+        box2d.init();
+        // declare a new current level object
+        game.currentLevel = {number:number,hero:[]};
+        game.score=0;
+        $('#score').html('Score: ' + game.score);
+        game.currentHero = undefined;
+        var level = levels.data[number];
+        //load the background, foreground, and slingshot images
+        game.currentLevel.backgroundImage =
+        loader.loadImage("images/backgrounds/" + level.background + ".png");
+        game.currentLevel.foregroundImage =
+        loader.loadImage("images/backgrounds/" + level.foreground + ".png");
+        game.slingshotImage = loader.loadImage("images/slingshot.png");
+        game.slingshotFrontImage = loader.loadImage("images/slingshot-front.png");
+        // Load all the entities
+        for (var i = level.entities.length - 1; i >= 0; i--) {
+            var entity = level.entities[i];
+            entities.create(entity);
+        };
+        //Call game.start() once the assets have loaded
+        if (loader.loaded) {
+            game.start()
+        } else {
+            loader.onload = game.start;
+        }
+    }
+
+}
---
+++ trunk/Game engine experiments/Physics Engine Basics/Physics Engine Basics/script/box2d.js
@@ -0,0 +1,185 @@
+﻿
+
+    // Declare all the commonly used objects as variables for convenience
+    var b2Vec2 = Box2D.Common.Math.b2Vec2;
+    var b2BodyDef = Box2D.Dynamics.b2BodyDef;
+    var b2Body = Box2D.Dynamics.b2Body;
+    var b2FixtureDef = Box2D.Dynamics.b2FixtureDef;
+    var b2Fixture = Box2D.Dynamics.b2Fixture;
+    var b2World = Box2D.Dynamics.b2World;
+    var b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape;
+    var b2CircleShape = Box2D.Collision.Shapes.b2CircleShape;
+    var b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
+    var b2RevoluteJointDef = Box2D.Dynamics.Joints.b2RevoluteJointDef;
+
+    var world;
+    var scale = 50; // 50 pixels on our canvas correspond to 1 meter in the Box2d world
+    var worldHeight = 600;
+    var worldWidth = 800;
+
+    function init() {
+        // Set up the Box2d world that will do most of the physics calculation
+        var gravity = new b2Vec2(0, 9.8); // Declare gravity as 9.8 m/s^2 downward
+        var allowSleep = true; // Allow objects that are at rest to fall asleep and be excluded from calculations
+        world = new b2World(gravity, allowSleep);
+
+        createFloor();
+        createRectangularBody();
+        createNinja();
+
+        // Create contact listeners and track events
+        listenForContact();
+
+        setupDebugDraw("canvas");
+        animate();
+    }
+
+    // Setting Up a Box2D Animation Loop
+    var timeStep = 1 / 60;
+    // As per the Box2d manual, the suggested iteration count for Box2D is 8 for velocity and 3 for position.
+    var velocityIterations = 8;
+    var positionIterations = 3;
+    function animate() {
+        world.Step(timeStep, velocityIterations, positionIterations);
+        world.ClearForces();
+        world.DrawDebugData();
+        setTimeout(animate, timeStep);
+    }
+
+    function animate() {
+        world.Step(timeStep, velocityIterations, positionIterations);
+        world.ClearForces();
+        world.DrawDebugData();
+
+        // Custom Drawing
+        if (ninja) {
+            drawNinja();
+        }
+
+        //Kill Special Body if Dead
+        if (ninja && ninja.GetUserData().life <= 0) {
+            world.DestroyBody(ninja);
+            ninja = undefined;
+            console.log("The special body was destroyed");
+        }
+
+        setTimeout(animate, timeStep);
+    }
+
+    function createFloor() {
+        //A body definition holds all the data needed to construct a rigid body.
+        var bodyDef = new b2BodyDef;
+        bodyDef.type = b2Body.b2_staticBody;
+        bodyDef.position.x = worldWidth / 2 / scale;
+        bodyDef.position.y = (worldHeight - 15) / scale;
+        // A fixture is used to attach a shape to a body for collision detection.
+        // A fixture definition is used to create a fixture.
+        var fixtureDef = new b2FixtureDef;
+        fixtureDef.density = 1.0; // used to calc weight of the body
+        fixtureDef.friction = 0.5; // make sure the body slides realistically
+        fixtureDef.restitution = 0.2; // bouncing value
+        fixtureDef.shape = new b2PolygonShape;
+        fixtureDef.shape.SetAsBox(worldWidth / 2 / scale, 15 / scale); // worldWidth pixels wide and 20 pixels tall
+        var body = world.CreateBody(bodyDef);
+        var fixture = body.CreateFixture(fixtureDef);
+    }
+
+    var context;
+    function setupDebugDraw(canvasName) {
+        var debugCanvas = document.getElementById(canvasName);
+        console.log(debugCanvas);
+        context = debugCanvas.getContext('2d');
+        var debugDraw = new b2DebugDraw();
+        // Use this canvas context for drawing the debugging screen
+        debugDraw.SetSprite(context);
+        // Set the scale
+        debugDraw.SetDrawScale(scale);
+        // Fill boxes with an alpha transparency of 0.3
+        debugDraw.SetFillAlpha(0.3);
+        // Draw lines with a thickness of 1
+        debugDraw.SetLineThickness(1.0);
+        // Display all shapes and joints
+        debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
+        // Start using debug draw in our world
+        world.SetDebugDraw(debugDraw);
+    }
+
+    function createRectangularBody(){
+        var bodyDef = new b2BodyDef;
+        bodyDef.type = b2Body.b2_dynamicBody;
+        bodyDef.position.x = 40 / scale;
+        bodyDef.position.y = 100 / scale;
+        var body = world.CreateBody(bodyDef);
+
+        var fixtureDef = new b2FixtureDef;
+        fixtureDef.density = 1.0;
+        fixtureDef.friction = 0.5;
+        fixtureDef.restitution = 0.3;
+        fixtureDef.shape = new b2PolygonShape;
+        fixtureDef.shape.SetAsBox(30/scale,50/scale);
+        var fixture = body.CreateFixture(fixtureDef);
+    }
+
+    var ninja;
+    function createNinja(){
+        var bodyDef = new b2BodyDef;
+        bodyDef.type = b2Body.b2_dynamicBody;
+        bodyDef.position.x = 155 / scale;
+        bodyDef.position.y = 0 / scale;
+        ninja = world.CreateBody(bodyDef);
+
+        var fixtureDef = new b2FixtureDef;
+        fixtureDef.density = 1.0;
+        fixtureDef.friction = 0.5;
+        fixtureDef.restitution = 0.5;
+        fixtureDef.shape = new b2PolygonShape;
+        fixtureDef.shape.SetAsBox(30 / scale, 50 / scale);
+        var fixture = ninja.CreateFixture(fixtureDef);
+
+        ninja.SetUserData({name:"special",life:250})
+    }
+
+    function listenForContact(){
+        var listener = new Box2D.Dynamics.b2ContactListener;
+        listener.PostSolve = function(contact,impulse){
+            var body1 = contact.GetFixtureA().GetBody();
+            var body2 = contact.GetFixtureB().GetBody();
+            // If either of the bodies is the special body, reduce its life
+            if (body1 == ninja || body2 == ninja){
+                var impulseAlongNormal = impulse.normalImpulses[0];
+                ninja.GetUserData().life -= impulseAlongNormal;
+                console.log("The special body was in a collision with impulse", impulseAlongNormal, "and its life has now become ", ninja.GetUserData().life);
+            }
+        };
+        world.SetContactListener(listener);
+    }
+
+    function drawNinja() {
+        // Get body position and angle
+        var position = ninja.GetPosition();
+        var angle = ninja.GetAngle();
+        // Translate and rotate axis to body position and angle
+        context.translate(position.x * scale, position.y * scale);
+        context.rotate(angle);
+        // Draw a filled circular face
+        context.fillStyle = "rgb(200,150,250);";
+        context.beginPath();
+        context.arc(0, 0, 30, 0, 2 * Math.PI, false);
+        context.fill();
+        // Draw two rectangular eyes
+        context.fillStyle = "rgb(255,255,255);";
+        context.fillRect(-15, -15, 10, 5);
+        context.fillRect(5, -15, 10, 5);
+        // Draw an upward or downward arc for a smile depending on life
+        context.strokeStyle = "rgb(255,255,255);";
+        context.beginPath();
+        if (ninja.GetUserData().life > 100) {
+            context.arc(0, 0, 10, Math.PI, 2 * Math.PI, true);
+        } else {
+            context.arc(0, 10, 10, Math.PI, 2 * Math.PI, false);
+        }
+        context.stroke();
+        // Translate and rotate axis back to original position and angle
+        context.rotate(-angle);
+        context.translate(-position.x * scale, -position.y * scale);
+    }
