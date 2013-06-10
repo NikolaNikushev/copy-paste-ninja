08c6be19b81f44a8d6407ed64fe0c621a1afed52
nikita.moshensky <nikita.moshensky@gmail.com>
2013-06-10 12:13:06 +0000

    basic experiments

---
+++ Game engine experiments/Physics Engine Basics/Integrating Physics Engine/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Integrating_Physics_Engine")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Integrating_Physics_Engine")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a38d28e8-ac9b-45d9-a5fc-f31079145740")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Game engine experiments/Physics Engine Basics/Integrating Physics Engine/script/entities.js
@@ -0,0 +1,85 @@
+﻿var entities = {
+    definitions: {
+        "glass": {
+            fullHealth: 100,
+            density: 2.4,
+            friction: 0.4,
+            restitution: 0.15,
+        },
+        "wood": {
+            fullHealth: 500,
+            density: 0.7,
+            friction: 0.4,
+            restitution: 0.4,
+        },
+        "dirt": {
+            density: 3.0,
+            friction: 1.5,
+            restitution: 0.2,
+        },
+        "card": {
+            shape: "circle",
+            fullHealth: 40,
+            radius: 5,
+            density: 1,
+            friction: 0.5,
+            restitution: 0.4,
+        },
+        "ninja": {
+            shape: "rectangle",
+            fullHealth: 80,
+            width: 30,
+            height: 50,
+            density: 1,
+            friction: 0.5,
+            restitution: 0.7,
+        },
+    },
+
+    // take the entity, create a Box2D body, and add it to the world
+    create: function (entity) {
+        var definition = entities.definitions[entity.name];
+        if(!definition){
+            console.log ("Undefined entity name", entity.name);
+            return;
+        }
+
+        switch (entity.type) {
+            case "block": // simple rectangles
+                entity.health = definition.fullHealth;
+                entity.fullHealth = definition.fullHealth;
+                entity.shape = "rectangle";
+                entity.sprite = loader.loadImage("images/"+entity.name+".png");
+                box2d.createRectangle(entity,definition);
+                break;
+            case "ground": // simple rectangles
+                // No need for health. These are indestructible
+                entity.shape = "rectangle";
+                // No need for sprites. These won't be drawn at all
+                box2d.createRectangle(entity,definition);
+                break;
+            case "hero": // simple circles
+            case "villain": // can be circles or rectangles
+                entity.health = definition.fullHealth;
+                entity.fullHealth = definition.fullHealth;
+                entity.sprite = loader.loadImage("images/"+entity.name+".png");
+                entity.shape = definition.shape;
+                if(definition.shape == "circle"){
+                    entity.radius = definition.radius;
+                    box2d.createCircle(entity,definition);
+                } else if (definition.shape == "rectangle") {
+                    entity.width = definition.width;
+                    entity.height = definition.height;
+                    box2d.createRectangle(entity, definition);
+                }
+                break;
+            default:
+                console.log("Undefined entity type", entity.type);
+                break;
+        }
+    },
+
+    // take the entity, its position, and its angle and draw it on the game canvas
+    draw: function (entity, position, angle) {
+    }
+}
