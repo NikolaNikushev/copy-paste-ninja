12ddcda82899bc415b6d46fe419df1bf5953a6d1
nickey.kolev@gmail.com <nickey.kolev@gmail.com@bab1d769-ab47-f41f-4f00-7e570b04f98a>
2013-06-16 13:13:32 +0000

    Scene 1 rearranged

--- trunk/Copy Paste Ninja/Game/scripts/levelsdata.js
+++ trunk/Copy Paste Ninja/Game/scripts/levelsdata.js
@@ -12,8 +12,12 @@ var levelsData = [
                 { type: "ground", name: "dirt", x: 4000 - 15, y: 600 / 2, width: 30, height: 600, isStatic: true },
 
                 // platforms
-                { type: "block", name: "platform", x: 320, y: 420, width: 400, height: 30, isStatic: true },
-                { type: "block", name: "platform", x: 700, y: 500, width: 200, height: 30, isStatic: true },
+                // 
+                { type: "block", name: "platform", x: 250, y: 500, width: 150, height: 30, isStatic: true },
+                { type: "block", name: "platform", x: 720, y: 420, width: 350, height: 30, isStatic: true },
+                { type: "block", name: "platform", x: 1100, y: 500, width: 200, height: 30, isStatic: true },
+                { type: "block", name: "platform", x: 1350, y: 420, width: 200, height: 30, isStatic: true },
+                { type: "block", name: "platform", x: 1600, y: 500, width: 200, height: 30, isStatic: true },
 
                 // obstacles
                 //{ type: "block", name: "box", x: 320, y: 375, width: 70, height: 50 },
@@ -22,8 +26,9 @@ var levelsData = [
 
                 // code
                 { type: "code", name: "code", x: 400, y: 290, width: 45, height: 30, isStatic: true, dead: false, score: 10, isSensor: true },
+                { type: "code", name: "code", x: 300, y: 390, width: 45, height: 30, isStatic: true, dead: false, score: 10, isSensor: true },
                 { type: "code", name: "code", x: 400, y: 550, width: 45, height: 30, isStatic: true, dead: false, score: 10, isSensor: true },
-                { type: "code", name: "code", x: 500, y: 390, width: 45, height: 30, isStatic: true, dead: false, score: 10, isSensor: true },
+                { type: "code", name: "code", x: 800, y: 550, width: 45, height: 30, isStatic: true, dead: false, score: 10, isSensor: true },
 
                 // complex shapes, defined by polygons
                 //{
@@ -39,9 +44,9 @@ var levelsData = [
                 // hero
                 { type: "hero", name: "ninja", x: 50, y: 410 },
                 //{ type: "hero", name: "badninja", x: 550, y: 410 },
-                { type: "hero", name: "nakov", x: 150, y: 510, minLeft: 100, maxRight: 400 },
-                { type: "hero", name: "niki", x: 650, y: 210, minLeft: 350, maxRight: 1100 },
-                { type: "hero", name: "doncho", x: 200, y: 210, minLeft: 120, maxRight: 400 },
+                { type: "hero", name: "nakov", x: 200, y: 510, minLeft: 200, maxRight: 850 },
+                { type: "hero", name: "doncho", x: 700, y: 210, minLeft: 610, maxRight: 790 },
+                { type: "hero", name: "niki", x: 1200, y: 210, minLeft: 1100, maxRight: 1800 },
             ]
         },
 
